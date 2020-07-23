451a7933816020f6962320daabe16b93db265f24
cpthooke <stevenhooke@hotmail.co.uk>
2020-07-23 19:00:51 +0100

    Syncing Git + Some Game
    - Prefabs.
    - Basic sprites.
    - Player movement.
    - Bullet movement.

---
+++ Assets/Scripts/bulletController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class bulletController : MonoBehaviour
+{
+    private Transform bullet;
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bullet = GetComponent<Transform>();
+    }
+
+    void FixedUpdate() //Updates from speed, not frame. 
+    {
+        bullet.position += Vector3.up * speed; //Upward directions.
+
+        if (bullet.position.y >= 5.3)
+            Destroy(gameObject); //Destroys bullet off screen.
+                
+    }
+
+    // Update is called once per frame
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Enemy")
+        {
+            Destroy(other.gameObject);
+            Destroy(gameObject);
+            //Increase player score later. @15:30
+        }
+        else if (other.tag == "Base")
+            Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/playerMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+    private Transform player;
+    public float speed;
+    public float maxBound, minBound;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GetComponent <Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxisRaw("Horizontal"); // A/D or Arrow keys.
+
+        if (player.position.x < minBound && h < 0)
+            h = 0; //If player is at far left, stops player going off screen.
+        else if (player.position.x > maxBound && h > 0)
+            h = 0;
+
+        player.position += Vector3.right * h * speed;
+        
+
+    }
+}
